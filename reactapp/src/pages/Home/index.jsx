7bc57e4448392da675d183e2252fdbd243ca855b
mykewayner <mykewayner@gmail.com>
2022-06-24 13:48:09 -0300

    Criada função de estado para o nome

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -1,16 +1,21 @@
+import React, { useState } from 'react';
 import './styles.css'
 
 import { Card } from '../../components/Card'
 
 function Home() {
+  const [studentName, setStudentName] = useState();
   return (
     <>
       <div className="container">
       <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome..."/>
+      <h1>Nome: {studentName}</h1>
+
+      <input type="text" placeholder="Digite o nome..." onChange={e => setStudentName(e.target.value)}/>
        <button type="button">Adicionar</button>
 
-       <Card />
+       <Card name="Rodrigo" time="10:25:23" />
+       <Card name="João" time="12:25:23" />
       </div>    
     </>
   )

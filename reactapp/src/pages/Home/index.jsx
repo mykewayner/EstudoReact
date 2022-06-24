54c2111ce5ca5dc673c9727707b4273c909bae38
mykewayner <mykewayner@gmail.com>
2022-06-24 14:32:50 -0300

    Criada função para criação dos cards

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -4,18 +4,30 @@ import './styles.css'
 import { Card } from '../../components/Card'
 
 function Home() {
-  const [studentName, setStudentName] = useState();
+  const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent(){
+    const newStudent = {
+       name: studentName, 
+       time: new Date().toLocaleTimeString("pt-br", {hour: '2-digit', minute: '2-digit', second: '2-digit'}) 
+  
+       };
+    
+       setStudents([newStudent]);
+  }
   return (
     <>
       <div className="container">
-      <h1>Lista de Presença</h1>
-      <h1>Nome: {studentName}</h1>
+        <h1>Lista de Presença</h1>
+        <h1>Nome: {studentName}</h1>
 
-      <input type="text" placeholder="Digite o nome..." onChange={e => setStudentName(e.target.value)}/>
-       <button type="button">Adicionar</button>
+        <input type="text" placeholder="Digite o nome..." onChange={e => setStudentName(e.target.value)}/>
+        <button type="button" onClick={handleAddStudent}>Adicionar</button>
 
-       <Card name="Rodrigo" time="10:25:23" />
-       <Card name="João" time="12:25:23" />
+       {
+        students.map(student => <Card name={student.name} time={student.time} /> )
+        }
       </div>    
     </>
   )

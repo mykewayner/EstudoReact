76afc66ae6da6665ab3d15e476fcad4a50bf3444
mykewayner <mykewayner@gmail.com>
2022-06-24 02:18:19 -0300

    Criado componente de card

---
+++ reactapp/src/components/Card/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+export function Card(){
+
+    return(
+        <div className='card'>
+            <strong>Rodrigo Gonçalves</strong>
+            <small>10:15:32</small>
+        </div>
+    )
+
+
+
+}

5a5c6b9707fe0eb8265a773649459c4f46df2368
mykewayner <mykewayner@gmail.com>
2022-06-24 02:18:07 -0300

    Criada página principal

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -1,10 +1,17 @@
+import './styles.css'
+
+import { Card } from '../../components/Card'
 
 function Home() {
   return (
     <>
-    <h1>Lista de Presença</h1>
-    <input type="text" placeholder="Digite o nome..."/>
-    <button type="button">Adicionar</button>
+      <div className="container">
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite o nome..."/>
+       <button type="button">Adicionar</button>
+
+       <Card />
+      </div>    
     </>
   )
 }
